12e8cc2d65007c0952835ac7d2bcd9184dea27be
madhan <53962076+maddyexplore@users.noreply.github.com>
2020-07-05 14:08:35 +0530

    Add files via upload

---
+++ place/place/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import stdb , stadb , job1
+
+# Register your models here.
+admin.site.register(stdb)
+admin.site.register(stadb)
+admin.site.register(job1)
+
